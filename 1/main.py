08ebd4ebd7894783e0dd8c5dc0a4541330b66fe4
Zer0k/On1 <jubueno@unal.edu.co>
2025-05-03 18:19:50 -0500

    Ejercicio 5

--- 1/main.py
+++ 1/main.py
@@ -1,9 +1,9 @@
 # 1) ejercicio 2.1 Página 63 definicion de clases
 class persona: 
-    nombre = ""
-    apellidos = ""
-    documento_identidad = ""
-    año_born = 0
+    nombre = str
+    apellidos = str
+    documento_identidad = str
+    año_born = int
     
     #el Constructor :)
     def __init__(self, nombre:str, apellidos:str, documento_identidad:str, año_born:int): 
@@ -17,11 +17,11 @@ persona_1 = persona("Alejandro","Restrepo García","1234567890",2007)
 persona_2 = persona("Juan","Bueno García","1234567890",2006)
 
 #funcion imprimir
-def imprimir(p):
-    print("Nombre: ", p.nombre)
-    print("Apellidos: ", p.apellidos)
-    print("Número de Documento de identidad: ", p.documento_identidad)
-    print("Año de nacimiento: ", p.año_born)
+def imprimir(self):
+    print("Nombre: ", self.nombre)
+    print("Apellidos: ", self.apellidos)
+    print("Número de Documento de identidad: ", self.documento_identidad)
+    print("Año de nacimiento: ", self.año_born)
     print("---------------")
 
 imprimir(persona_1)
